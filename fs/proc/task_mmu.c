96a0df800da822482ecea7afb63fa2635f4ce41f
huangzq2 <huangzq2@motorola.com>
2024-10-08 18:49:38 +0800

    abort reclaim once process goes to foreground
    App compaction can not be aborted due to current design,
    it causes app get stuck for a while when it resume to foreground
    if app is compacting by kernel. So now we add check in kernel
    while loop to abort the compaction if adj is zero.
    
    Change-Id: I16f36a08d1a5efcb36827e84f5058abfa0a7dae0
    Signed-off-by: huangzq2 <huangzq2@motorola.com>
    Reviewed-on: https://gerrit.mot.com/2014822
    SME-Granted: SME Approvals Granted
    SLTApproved: Slta Waiver
    Tested-by: Jira Key
    Reviewed-by: Xiangpo Zhao <zhaoxp3@motorola.com>
    Submit-Approved: Jira Key
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -2121,6 +2121,9 @@ static ssize_t reclaim_write(struct file *file, const char __user *buf,
 	if (type == RECLAIM_RANGE) {
 		vma = find_vma(mm, start);
 		while (vma) {
+            /* Moto huangzq2: abort reclaim if app goes to foreground. */
+            if (task->signal->oom_score_adj == 0)
+                    break;
 			if (vma->vm_start > end)
 				break;
 			if (is_vm_hugetlb_page(vma))
@@ -2134,6 +2137,9 @@ static ssize_t reclaim_write(struct file *file, const char __user *buf,
 		}
 	} else {
 		for (vma = mm->mmap; vma; vma = vma->vm_next) {
+			/* Moto huangzq2: abort reclaim if app goes to foreground. */
+			if (task->signal->oom_score_adj == 0)
+                break;
 			if (is_vm_hugetlb_page(vma))
 				continue;
 
